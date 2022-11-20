c05969284616ead98b7bb68135e09679d841ac93
Fisnik <fisnik.duraku@student.vinci.be>
2022-11-20 16:36:52 +0100

    refactor: roll loop duplicated
    adding fonction that rolls for every tests so we dont have the for loop in every test

--- src/__test__/bowlings.test.js
+++ src/__test__/bowlings.test.js
@@ -2,22 +2,21 @@ const Game = require('../bowling')
 
 const game = new Game(0)
 
+function rollMany(rolls, pins) {
+  for (let i = 0; i < rolls; i++)
+  game.roll(pins);
+}
+
 describe('score equal to zero when no pins down - score()', () => {
   test('should return value : 0', () => {
-    for (let i = 0; i < 20; i++) {
-      game.roll(0)
-    }
-
+   rollMany(20,0);
     expect(game.scores()).toBe(0)
   })
 })
 
 describe(' score equal 20 when 1 pin down at each roll', () => {
   test('should return value : 20', () => {
-    for (let i = 0; i < 20; i++) {
-      game.roll(1)
-    }
-
+    rollMany(20,1);
     expect(game.scores()).toBe(20)
   })
 })
